e8d55b4c9e2dc15e7e2db86cff634f177b603f13
OmGhag <omghag2003@gmail.com>
2026-04-17 19:28:35 -0700

    declaring Phase policies for councils

---
+++ packages/council_policies/src/council_policies/policies/__init__.py
@@ -0,0 +1,7 @@
+# policies sub-package
+from council_policies.policies.p1 import P1Policy
+from council_policies.policies.p2 import P2Policy
+from council_policies.policies.p3 import P3Policy
+from council_policies.policies.p4 import P4Policy
+
+__all__ = ["P1Policy", "P2Policy", "P3Policy", "P4Policy"]
